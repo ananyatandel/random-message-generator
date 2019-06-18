613f4887b4a33852a5482eb1c4c5e4d526f3bc9f
Apple <apple@Apples-MacBook-Pro-4.local>
2019-06-18 10:56:10 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var 5AM = $('.5am');
+var 7AM = $('.7am');
+var 9AM = $('.9am');
+var Sleep = $('.Sleep');
+var Stay = $('.Stay');
+var Out = $('.Out');
+var Gym = $('.Gym');
+var Work = $('.Work');
+var Shop = $('.Shop');
+var Other = $('.Other');
+var But = $('.enter');
+
+But.on("click", Switch);
+function Switch(){
+  Body.append
+}
